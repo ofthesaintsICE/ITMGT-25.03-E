337e3aa8b0910e1fecfe8afda4d57b516d1c6a66
unknown <isaacfrancodlssnts@gmail.com>
2025-07-01 20:57:49 +0800

    programming set 3

--- set_3.py
+++ set_3.py
@@ -4,98 +4,44 @@ This assignment will develop your ability to manipulate data.
 '''
 
 def relationship_status(from_member, to_member, social_graph):
-    '''Relationship Status.
-
-    Let us pretend that you are building a new app.
-    Your app supports social media functionality, which means that users can have
-    relationships with other users.
-
-    There are two guidelines for describing relationships on this social media app:
-    1. Any user can follow any other user.
-    2. If two users follow each other, they are considered friends.
-
-    This function describes the relationship that two users have with each other.
-
-    Please see "set_3_sample_data.py" for sample data. The social graph
-    will adhere to the same pattern.
-
-    Parameters
-    ----------
-    from_member: str
-        the subject member
-    to_member: str
-        the object member
-    social_graph: dict
-        the relationship data
-
-    Returns
-    -------
-    str
-        "follower" if from_member follows to_member,
-        "followed by" if from_member is followed by to_member,
-        "friends" if from_member and to_member follow each other,
-        "no relationship" if neither from_member nor to_member follow each other.
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
-
+    if (from_member in social_graph[to_member]['following']) and (to_member in social_graph[from_member]['following']):
+        return 'friends'
+    elif from_member in social_graph[to_member]['following']:
+        return 'followed by'
+    elif to_member in social_graph[from_member]['following']:
+        return 'follower'
+    else:
+        return 'no relationship'
 
 def tic_tac_toe(board):
-    '''Tic Tac Toe.
+    board_size = len(board)
 
-    Tic Tac Toe is a common paper-and-pencil game.
-    Players must attempt to successfully draw a straight line of their symbol across a grid.
-    The player that does this first is considered the winner.
+    for row in board:
+        if row[0] != '' and all(cell == row[0] for cell in row):
+            return row[0]
 
-    This function evaluates a tic tac toe board and returns the winner.
+    for column in range(board_size):
+        if board[0][column] != '' and all(board[row][column] == board[0][column] for row in range(board_size)):
+            return board[0][column]
 
-    Please see "set_3_sample_data.py" for sample data. The board will adhere
-    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
-    have more than one winner. The board will only ever have 2 unique symbols at the same time.
+    if board[0][0] != '' and all(board[i][i] == board[0][0] for i in range(board_size)):
+        return board[0][0]
 
-    Parameters
-    ----------
-    board: list
-        the representation of the tic-tac-toe board as a square list of lists
+    if board[0][board_size - 1] != '' and all(board[i][board_size - 1 - i] == board[0][board_size - 1] for i in range(board_size)):
+        return board [0][board_size - 1]
 
-    Returns
-    -------
-    str
-        the symbol of the winner or "NO WINNER" if there is no winner
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    return 'NO WINNER'
+
 
 def eta(first_stop, second_stop, route_map):
-    '''ETA.
+    time = 0
+    current_stop = first_stop
 
-    A shuttle van service is tasked to travel along a predefined circlar route.
-    This route is divided into several legs between stops.
-    The route is one-way only, and it is fully connected to itself.
+    while current_stop != second_stop:
+        for (start, end), data in route_map.items():
+            if start == current_stop:
+                time += data['travel_time_mins']
+                current_stop = end
+                break
 
-    This function returns how long it will take the shuttle to arrive at a stop
-    after leaving another stop.
-
-    Please see "set_3_sample_data.py" for sample data. The route map will
-    adhere to the same pattern. The route map may contain more legs and more stops,
-    but it will always be one-way and fully enclosed.
-
-    Parameters
-    ----------
-    first_stop: str
-        the stop that the shuttle will leave
-    second_stop: str
-        the stop that the shuttle will arrive at
-    route_map: dict
-        the data describing the routes
-
-    Returns
-    -------
-    int
-        the time it will take the shuttle to travel from first_stop to second_stop
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    return time
